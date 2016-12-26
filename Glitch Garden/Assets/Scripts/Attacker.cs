affb1a96df27365c18014e14f6fa772bbd74439c
mahyar121 <mahyarhajibabaie@gmail.com>
2016-12-26 14:09:32 -0800

    Added attacker and defender script

---
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range (-1f, 1.5f)]
+    public float walkSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+        myRigidbody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // moves object to the left
+        transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+}
