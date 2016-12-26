81983471080c612467d7cc803030f4a718acc5f9
mahyar121 <mahyarhajibabaie@gmail.com>
2016-12-25 17:34:48 -0800

    Added Splash Screen loading

---
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public float autoLoadNextLevelAfter;
+
+    private void Start()
+    {
+        // used for Splash Screen loading
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Application.Quit();
+    }
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+  
+
+}
