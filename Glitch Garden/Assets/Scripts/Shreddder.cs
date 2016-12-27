0c0ca7e12fef248bfaaa46862bfff8cf99988799
Mahyar Babaie <mahyarhajibabaie@gmail.com>
2016-12-26 23:33:24 -0800

    Update Shreddder.cs

--- Glitch Garden/Assets/Scripts/Shreddder.cs
+++ Glitch Garden/Assets/Scripts/Shreddder.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Shreddder : MonoBehaviour {
+public class Shreddder : MonoBehaviour 
+{
 
-	void OnTriggerEnter2D (Collider2D collider) {
+	void OnTriggerEnter2D (Collider2D collider) 
+	{
 		Destroy (collider.gameObject);	
 	}
 	
