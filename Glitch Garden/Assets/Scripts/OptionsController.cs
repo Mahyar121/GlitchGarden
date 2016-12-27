d166f1e020585e31ac4e867ff6f52d164dc37cc9
Mahyar Babaie <mahyarhajibabaie@gmail.com>
2016-12-26 23:31:11 -0800

    Update OptionsController.cs

--- Glitch Garden/Assets/Scripts/OptionsController.cs
+++ Glitch Garden/Assets/Scripts/OptionsController.cs
@@ -1,8 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class OptionsController : MonoBehaviour {
+public class OptionsController : MonoBehaviour
+{
 
 	public Slider volumeSlider, diffSlider;
 	public LevelManager levelManager;
@@ -10,24 +11,28 @@ public class OptionsController : MonoBehaviour {
 	private MusicManager musicManager;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		musicManager = GameObject.FindObjectOfType<MusicManager>();
 		volumeSlider.value = PlayerPrefsManager.GetMasterVolume ();
 		diffSlider.value = PlayerPrefsManager.GetDifficulty ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		musicManager.SetVolume (volumeSlider.value);
 	}
 	
-	public void SaveAndExit () {
+	public void SaveAndExit () 
+	{
 		PlayerPrefsManager.SetMasterVolume (volumeSlider.value);
 		PlayerPrefsManager.SetDifficulty (diffSlider.value);
 		levelManager.LoadLevel ("01a Start");
 	}
 	
-	public void SetDefaults () {
+	public void SetDefaults () 
+	{
 		volumeSlider.value = 0.8f;
 		diffSlider.value = 2f;
 	}
