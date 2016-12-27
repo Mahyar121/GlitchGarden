4e7a29b5e9e7916fab66058c63f5597fae4b9b29
Mahyar Babaie <mahyarhajibabaie@gmail.com>
2016-12-26 23:30:32 -0800

    Update MusicManager.cs

--- Glitch Garden/Assets/Scripts/MusicManager.cs
+++ Glitch Garden/Assets/Scripts/MusicManager.cs
@@ -1,34 +1,40 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class MusicManager : MonoBehaviour {
+public class MusicManager : MonoBehaviour 
+{
 
 	public AudioClip[] levelMusicChangeArray;
 
 	private AudioSource audioSource;
 
-	void Awake() {
+	void Awake() 
+	{
 		DontDestroyOnLoad (gameObject);
 		Debug.Log ("Don't destory on load: " + name);
 	}
 	
-	void Start () {
+	void Start () 
+	{
 		audioSource = GetComponent<AudioSource>();
 		audioSource.volume = PlayerPrefsManager.GetMasterVolume();
 	}
 	
-	void OnLevelWasLoaded (int level) {
+	void OnLevelWasLoaded (int level) 
+	{
 		AudioClip thisLevelMusic = levelMusicChangeArray[level];
 		Debug.Log ("Playing clip: " + thisLevelMusic);
 		
-		if (thisLevelMusic) { // If there's some music attached
+		if (thisLevelMusic) 
+		{ // If there's some music attached
 			audioSource.clip = thisLevelMusic;
 			audioSource.loop = true;
 			audioSource.Play ();
 		}
 	}
 	
-	public void SetVolume (float volume) {
+	public void SetVolume (float volume) 
+	{
 		audioSource.volume = volume;
 	}
 }
