a1e0558218446af9997ae6af2cd9b608d0432e4c
Mahyar Babaie <mahyarhajibabaie@gmail.com>
2016-12-26 23:28:23 -0800

    Update Health.cs

--- Glitch Garden/Assets/Scripts/Health.cs
+++ Glitch Garden/Assets/Scripts/Health.cs
@@ -1,19 +1,23 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class Health : MonoBehaviour {
+public class Health : MonoBehaviour 
+{
 	
 	public float health = 100f;
 	
-	public void DealDamage (float damage) {
+	public void DealDamage (float damage) 
+	{
 		health -= damage;
-		if (health < 0) {
+		if (health < 0) 
+		{
 			// Optionally trigger animation
 			DestoryObject ();
 		}
 	}
 	
-	public void DestoryObject () {
+	public void DestoryObject () 
+	{
 		Destroy (gameObject);
 	}
 	
