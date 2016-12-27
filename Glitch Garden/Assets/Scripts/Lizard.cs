f68b6f811d19c8f3298c8e05271c98022e790ef1
Mahyar Babaie <mahyarhajibabaie@gmail.com>
2016-12-26 23:29:24 -0800

    Update Lizard.cs

--- Glitch Garden/Assets/Scripts/Lizard.cs
+++ Glitch Garden/Assets/Scripts/Lizard.cs
@@ -1,28 +1,27 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 [RequireComponent (typeof (Attacker))]
-public class Lizard : MonoBehaviour {
+public class Lizard : MonoBehaviour 
+{
 	
 	private Animator anim;
 	private Attacker attacker;
 	
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		anim = GetComponent<Animator>();
 		attacker = GetComponent<Attacker>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
-	void OnTriggerEnter2D (Collider2D collider) {
+	void OnTriggerEnter2D (Collider2D collider) 
+	{
 		GameObject obj = collider.gameObject;
 		
 		// Leave the method if not colliding with defender
-		if (!obj.GetComponent<Defender>()) {
+		if (!obj.GetComponent<Defender>()) 
+		{
 			return;
 		}
 		
