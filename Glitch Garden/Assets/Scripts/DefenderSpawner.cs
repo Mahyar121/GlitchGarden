51072027cbcde9062c40d5e030eeb3d4b093b4a4
Mahyar Babaie <mahyarhajibabaie@gmail.com>
2016-12-26 23:26:08 -0800

    Update DefenderSpawner.cs

--- Glitch Garden/Assets/Scripts/DefenderSpawner.cs
+++ Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -1,30 +1,37 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class DefenderSpawner : MonoBehaviour {
+public class DefenderSpawner : MonoBehaviour 
+{
 
 	public Camera myCamera;
 	private GameObject parent;
 	private StarDisplay starDisplay;
 	
-	void Start () {
+	void Start () 
+	{
 		parent = GameObject.Find ("Defenders");
 		starDisplay = GameObject.FindObjectOfType<StarDisplay>();
 		
-		if (!parent) {
+		if (!parent) 
+		{
 			parent = new GameObject("Defenders");
 		}
 	}
 	
-	void OnMouseDown () {
+	void OnMouseDown () 
+	{
 		Vector2 rawPos = CalculateWorldPointOfMouseClick();
 		Vector2 roundedPos = SnapToGrid (rawPos);
 		GameObject defender = Button.selectedDefender;
 		
 		int defenderCost = defender.GetComponent<Defender>().starCost;
-		if (starDisplay.UseStars(defenderCost) == StarDisplay.Status.SUCCESS) {
+		if (starDisplay.UseStars(defenderCost) == StarDisplay.Status.SUCCESS) 
+		{
 			SpawnDefender (roundedPos, defender);
-		} else {
+		} 
+		else 
+		{
 			Debug.Log ("Insufficient stars to spawn");
 		}
 	}
@@ -36,13 +43,15 @@ public class DefenderSpawner : MonoBehaviour {
 		newDef.transform.parent = parent.transform;
 	}
 	
-	Vector2 SnapToGrid (Vector2 rawWorldPos) {
+	Vector2 SnapToGrid (Vector2 rawWorldPos) 
+	{
 		float newX = Mathf.RoundToInt (rawWorldPos.x);
 		float newY = Mathf.RoundToInt (rawWorldPos.y);
 		return new Vector2 (newX, newY);
 	}
 	
-	Vector2 CalculateWorldPointOfMouseClick () {
+	Vector2 CalculateWorldPointOfMouseClick () 
+	{
 		float mouseX = Input.mousePosition.x;
 		float mouseY = Input.mousePosition.y;
 		float distanceFromCamera = 10f;
